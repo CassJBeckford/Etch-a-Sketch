c9f3c6d730405810b22526691dad96ad2ebd418a
cassjbeckford <cassjbeckford282@outlook.com>
2024-03-22 15:51:15 +0000

    create board function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+createBoard(16);
+} )
 
+function createBoard(size){
+  let container = document.querySelector(".container");
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
